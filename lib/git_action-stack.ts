0a4b1de9fb7a1543dd3e0ba898db57902127e9b6
Abdul <metalsquad4u@gmail.com>
2022-09-14 19:22:50 +0300

    Edited git_action file to test auto PR on new-branch branch

--- lib/git_action-stack.ts
+++ lib/git_action-stack.ts
@@ -8,7 +8,7 @@ import * as s3 from 'aws-cdk-lib/aws-s3'
 export class GitActionStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-/*
+
    const key = new kms.Key(this, 'github-action-kms-key', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       pendingWindow: cdk.Duration.days(7),
@@ -16,13 +16,13 @@ export class GitActionStack extends cdk.Stack {
       description: 'KMS key for encrypting the objects in an S3 bucket',
       enableKeyRotation: false,
     });
-    */
+    
 
     const s3Bucket = new s3.Bucket(this, 'my-bucket', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       encryption: s3.BucketEncryption.KMS,
       // 👇 encrypt with our KMS key
-     //encryptionKey: key,
+     encryptionKey: key,
     });
 
     //This is a test for automatic PR in the stack file
